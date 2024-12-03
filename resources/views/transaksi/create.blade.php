cbe06135161b23d32b2afb61469052ddec73da5a
luthfiabdllh <ahmadabdllh000@gmail.com>
2024-12-03 14:02:25 +0700

    transaksi create

--- resources/views/transaksi/create.blade.php
+++ resources/views/transaksi/create.blade.php
@@ -15,150 +15,76 @@
                     </ul>
                 </div>
             @endif
-            <form method="POST" action={{ route('transaksi.store') }}>
+            <form method="POST" action="{{ route('transaksi.store') }}">
                 @csrf
                 <div class="d-flex flex-column gap-4 mb-4">
                     <div class="form-group">
                         <label>Tanggal Pembelian</label>
-                        <input type="date" class="form-control" name="" value="{{ old('') }}" required>
+                        <input type="date" class="form-control" name="tanggal_pembelian" value="{{ old('tanggal_pembelian') }}" required>
                     </div>
                 </div>
                 <h6>Produk yang dibeli</h6>
                 <div class="accordion mb-4" id="accordionItem">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#item1" aria-expanded="true">
-                                Item #1
-                            </button>
-                        </h2>
-                        <div id="item1" class="accordion-collapse collapse show" data-bs-parent="#accordionItem">
-                            <div class="accordion-body">
-                                <div class="d-flex flex-column gap-4 mb-4">
-                                    <div class="form-group">
-                                        <label>Nama Produk</label>
-                                        <input type="text" class="form-control" name="nama_produk1" value="{{ old('') }}" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Harga Satuan</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="number" class="form-control" name="harga_satuan1" value="{{ old('') }}" required>
+                    @for ($i = 1; $i <= 3; $i++)
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button {{ $i == 1 ? '' : 'collapsed' }}" type="button" data-bs-toggle="collapse" data-bs-target="#item{{ $i }}" aria-expanded="{{ $i == 1 ? 'true' : 'false' }}">
+                                    Item #{{ $i }}
+                                </button>
+                            </h2>
+                            <div id="item{{ $i }}" class="accordion-collapse collapse {{ $i == 1 ? 'show' : '' }}" data-bs-parent="#accordionItem">
+                                <div class="accordion-body">
+                                    <div class="d-flex flex-column gap-4 mb-4">
+                                        <div class="form-group">
+                                            <label>Nama Produk</label>
+                                            <input type="text" class="form-control" name="nama_produk{{ $i }}" value="{{ old('nama_produk' . $i) }}" required>
                                         </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Jumlah</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="number" class="form-control" name="jumlah1" value="{{ old('') }}" required>
+                                        <div class="form-group">
+                                            <label>Harga Satuan</label>
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text">Rp</span>
+                                                <input type="number" class="form-control" name="harga_satuan{{ $i }}" value="{{ old('harga_satuan' . $i) }}" required>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Subtotal</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="text" class="form-control" name="subtotal1" value="{{ old('') }}" readonly>
+                                        <div class="form-group">
+                                            <label>Jumlah</label>
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text">Rp</span>
+                                                <input type="number" class="form-control" name="jumlah{{ $i }}" value="{{ old('jumlah' . $i) }}" required>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#item2" aria-expanded="false" aria-controls="item2">
-                                Item #2
-                            </button>
-                        </h2>
-                        <div id="item2" class="accordion-collapse collapse" data-bs-parent="#accordionItem">
-                            <div class="accordion-body">
-                                <div class="d-flex flex-column gap-4 mb-4">
-                                    <div class="form-group">
-                                        <label>Nama Produk</label>
-                                        <input type="text" class="form-control" name="nama_produk2" value="{{ old('') }}" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Harga Satuan</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="number" class="form-control" name="" value="{{ old('') }}" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Jumlah</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="number" class="form-control" name="" value="{{ old('') }}" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Subtotal</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="text" class="form-control" name="" value="{{ old('') }}" readonly>
+                                        <div class="form-group">
+                                            <label>Subtotal</label>
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text">Rp</span>
+                                                <input type="text" class="form-control" name="subtotal{{ $i }}" value="{{ old('subtotal' . $i) }}" readonly>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header">
-                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#item3" aria-expanded="false" aria-controls="item3">
-                                Item #3
-                            </button>
-                        </h2>
-                        <div id="item3" class="accordion-collapse collapse" data-bs-parent="#accordionItem">
-                            <div class="accordion-body">
-                                <div class="d-flex flex-column gap-4 mb-4">
-                                    <div class="form-group">
-                                        <label>Nama Produk</label>
-                                        <input type="text" class="form-control" name="nama_produk3" value="{{ old('') }}" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Harga Satuan</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="number" class="form-control" name="" value="{{ old('') }}" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Jumlah</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="number" class="form-control" name="" value="{{ old('') }}" required>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Subtotal</label>
-                                        <div class="input-group mb-3">
-                                            <span class="input-group-text">Rp</span>
-                                            <input type="text" class="form-control" name="" value="{{ old('') }}" readonly>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    @endfor
                 </div>
                 <div class="form-group">
                     <label>Harga Total</label>
                     <div class="input-group mb-3">
                         <span class="input-group-text">Rp</span>
-                        <input type="number" class="form-control" name="" value="{{ old('') }}" readonly>
+                        <input type="number" class="form-control" name="total_harga" value="{{ old('total_harga') }}" readonly>
                     </div>
                 </div>
                 <div class="form-group">
                     <label>Bayar</label>
                     <div class="input-group mb-3">
                         <span class="input-group-text">Rp</span>
-                        <input type="number" class="form-control" name="" value="{{ old('') }}" required>
+                        <input type="number" class="form-control" name="bayar" value="{{ old('bayar') }}" required>
                     </div>
                 </div>
                 <div class="form-group">
                     <label>Kembalian</label>
                     <div class="input-group mb-3">
                         <span class="input-group-text">Rp</span>
-                        <input type="text" class="form-control" name="" value="{{ old('') }}" readonly>
+                        <input type="text" class="form-control" name="kembalian" value="{{ old('kembalian') }}" readonly>
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Simpan</button>
@@ -166,8 +92,6 @@
         </div>
     </div>
 @endsection
-
-{{--customjs--}}
 
 @push('customjs')
 <script>
@@ -179,24 +103,24 @@
         };
 
         function calculateSubtotal(item) {
-            const hargaSatuan = parseInt($('input[name="harga_satuan'+item+'"]').val()) || 0;
-            const jumlah = parseInt($('input[name="jumlah'+item+'"]').val()) || 0;
-            subtotals['subtotal'+item] = hargaSatuan * jumlah;
-            $('input[name="subtotal'+item+'"]').val(subtotals['subtotal'+item]);
+            const hargaSatuan = parseInt($('input[name="harga_satuan' + item + '"]').val()) || 0;
+            const jumlah = parseInt($('input[name="jumlah' + item + '"]').val()) || 0;
+            subtotals['subtotal' + item] = hargaSatuan * jumlah;
+            $('input[name="subtotal' + item + '"]').val(subtotals['subtotal' + item]);
             $('input[name="total_harga"]').val(Object.values(subtotals).reduce((accumulator, currentValue) => accumulator + currentValue, 0));
         }
 
-        ('input[name="harga_satuan1"], input[name="jumlah1"]').on('', function() {
+        $('input[name="harga_satuan1"], input[name="jumlah1"]').on('input', function() {
             calculateSubtotal(1);
         });
-        $('input[name="harga_satuan2"], input[name="jumlah2"]').on('', function() {
+        $('input[name="harga_satuan2"], input[name="jumlah2"]').on('input', function() {
             calculateSubtotal(2);
         });
-        $('input[name="harga_satuan3"], input[name="jumlah3"]').on('', function() {
+        $('input[name="harga_satuan3"], input[name="jumlah3"]').on('input', function() {
             calculateSubtotal(3);
         });
 
-        $('input[name="bayar"]').on('', function() {
+        $('input[name="bayar"]').on('input', function() {
             const total_harga = parseInt($('input[name="total_harga"]').val()) || 0;
             const bayar = parseInt($('input[name="bayar"]').val()) || 0;
             const kembalian = bayar - total_harga;
