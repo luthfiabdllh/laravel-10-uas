c302936e0283d1a48f93af5a6efb26b796be04a7
luthfiabdllh <ahmadabdllh000@gmail.com>
2024-12-03 13:51:10 +0700

    login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="row justify-content-center mt-5">
     <div class="col-md-8">
-
         <div class="card">
             <div class="card-header">Login</div>
             <div class="card-body">
@@ -17,19 +16,19 @@
                     <div class="mb-3 row">
                         <label for="email" class="col-md-4 col-form-label text-md-end text-start">Email Address</label>
                         <div class="col-md-6">
-                            <input type="email" class="form-control @error('') is-invalid @enderror" name="" value="{{ old('') }}">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
-                            @endif
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}">
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="password" class="col-md-4 col-form-label text-md-end text-start">Password</label>
                         <div class="col-md-6">
-                            <input type="password" class="form-control @error('') is-invalid @enderror" name="">
-                            @if ($errors->has(''))
-                                <span class="text-danger">{{ $errors->first('') }}</span>
-                            @endif
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
+                            @error('password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
                         </div>
                     </div>
                     <div class="mb-3 row">
@@ -41,4 +40,3 @@
     </div>
 </div>
 @endsection
-
